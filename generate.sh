4323040c4e92f59603d5ca43ca459e06a954cff9
Hasan Noori <xishma@gmail.com>
2022-08-03 16:16:17 +0430

    update addresses

--- generate.sh
+++ generate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
  cd /files/spec/ && \
- wget -O icas-raw.yaml https://accounts.formaloo.net/docs/openapi/yaml/
- wget -O formz-raw.yaml https://api.formaloo.net/docs/openapi/yaml/
- wget -O actions-raw.yaml https://actions.formaloo.net/docs/openapi/yaml
- wget -O crm-raw.yaml https://cdpapi.formaloo.net/docs/openapi/yaml
+ wget -O icas-raw.yaml https://staging.icas.formaloo.com/docs/openapi/yaml/
+ wget -O formz-raw.yaml https://api.staging.formaloo.com/docs/openapi/yaml/
+ wget -O actions-raw.yaml https://staging.actions.formaloo.com/docs/openapi/yaml
+ wget -O crm-raw.yaml https://api.staging.crm.formaloo.com/docs/openapi/yaml
  redocly bundle icas-raw.yaml -o icas.yaml
  redocly bundle formz-raw.yaml -o formz.yaml
  redocly bundle actions-raw.yaml -o actions.yaml
@@ -12,11 +12,11 @@
  redocly bundle raw-v1.0.yaml -o v1.0.yaml
  npx openapi-merge-cli --config openapi-merge-v1.0.json
  rm actions* formz* icas* crm* storage* v1.0.yaml
- wget -O icas-raw.yaml https://accounts.formaloo.net/docs/openapi/yaml/?version=2.0
- wget -O formz-raw.yaml https://api.formaloo.net/docs/openapi/yaml/?version=2.0
- wget -O actions-raw.yaml https://actions.formaloo.net/docs/openapi/yaml?version=2.0
- wget -O crm-raw.yaml https://cdpapi.formaloo.net/docs/openapi/yaml?version=2.0
- wget -O storage-raw.yaml https://storage.formaloo.net/docs/openapi/yaml/?version=2.0
+ wget -O icas-raw.yaml https://staging.icas.formaloo.com/docs/openapi/yaml/?version=2.0
+ wget -O formz-raw.yaml https://api.staging.formaloo.com/docs/openapi/yaml/?version=2.0
+ wget -O actions-raw.yaml https://staging.actions.formaloo.com/docs/openapi/yaml?version=2.0
+ wget -O crm-raw.yaml https://api.staging.crm.formaloo.com/docs/openapi/yaml?version=2.0
+ wget -O storage-raw.yaml https://staging.storage.formaloo.com/docs/openapi/yaml/?version=2.0
  redocly bundle icas-raw.yaml -o icas.yaml
  redocly bundle formz-raw.yaml -o formz.yaml
  redocly bundle actions-raw.yaml -o actions.yaml
