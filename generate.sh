a2db9061aa9c52878eb70277100d0d430a8325b2
Hasan Noori <xishma@gmail.com>
2023-02-25 11:43:40 +0330

    adds new endpoints + authentication

--- generate.sh
+++ generate.sh
@@ -6,8 +6,9 @@ wget -O icas.yaml http://icas/docs/openapi/yaml/
 wget -O formz.yaml http://formz/docs/openapi/yaml/
 wget -O actions.yaml http://actions/docs/openapi/yaml
 wget -O crm.yaml http://crm/docs/openapi/yaml
+wget -O authentication.yaml http://authentication/docs/openapi/yaml
 
-files=( icas.yaml formz.yaml actions.yaml crm.yaml )
+files=( icas.yaml formz.yaml actions.yaml crm.yaml authentication.yaml )
 for file in "${files[@]}"; 
 do
     grep -o 'docs.*.md' $file | while read -r line ; 
@@ -20,19 +21,21 @@ redocly bundle icas.yaml -o icas-bundeled.yaml
 redocly bundle formz.yaml -o formz-bundeled.yaml
 redocly bundle actions.yaml -o actions-bundeled.yaml
 redocly bundle crm.yaml -o crm-bundeled.yaml
+redocly bundle authentication.yaml -o authentication-bundeled.yaml
 redocly bundle storage-v1.0.yaml -o storage-bundeled.yaml
 redocly bundle v1.0.yaml -o v1.0-bundeled.yaml
 
 npx openapi-merge-cli --config openapi-merge-v1.0.json
-rm actions* formz* icas* crm* storage-bundeled.yaml v1.0-bundeled.yaml
+rm actions* formz* icas* crm* authentication* storage-bundeled.yaml v1.0-bundeled.yaml
 
 wget -O icas.yaml http://icas/docs/openapi/yaml/?version=2.0
 wget -O formz.yaml http://formz/docs/openapi/yaml/?version=2.0
 wget -O actions.yaml http://actions/docs/openapi/yaml?version=2.0
 wget -O crm.yaml http://crm/docs/openapi/yaml?version=2.0
+wget -O authentication.yaml http://authentication/docs/openapi/yaml?version=2.0
 wget -O storage.yaml http://storage/docs/openapi/yaml/?version=2.0
 
-files=( icas.yaml formz.yaml actions.yaml crm.yaml storage.yaml )
+files=( icas.yaml formz.yaml actions.yaml crm.yaml storage.yaml authentication.yaml )
 for file in "${files[@]}"; 
 do
     grep -o 'docs.*.md' $file | while read -r line ; 
@@ -45,11 +48,12 @@ redocly bundle icas.yaml -o icas-bundeled.yaml
 redocly bundle formz.yaml -o formz-bundeled.yaml
 redocly bundle actions.yaml -o actions-bundeled.yaml
 redocly bundle crm.yaml -o crm-bundeled.yaml
+redocly bundle authentication.yaml -o authentication-bundeled.yaml
 redocly bundle storage.yaml -o storage-bundeled.yaml
 redocly bundle v2.0.yaml -o v2.0-bundeled.yaml
 
 npx openapi-merge-cli --config openapi-merge-v2.0.json
-rm actions* formz* icas* crm* storage-bundeled.yaml v2.0-bundeled.yaml 
+rm actions* formz* icas* crm* authentication* storage-bundeled.yaml v2.0-bundeled.yaml 
 
 mkdir -p /files/html/ && rm -r /files/html/*
 cp /files/v*.html /files/html/
---
+++ spec/docs/v2.0/end-users/domains/post.md
@@ -0,0 +1,3 @@
+# Get authentication domain
+
+By sending the share address or slug of your application as the `originator`, you can get its authentication domain (if any) to use for the auth actions.
