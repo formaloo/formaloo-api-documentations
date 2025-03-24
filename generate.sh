f525bbab07d0b43fb75e2d35e27508849dd2196e
Hasan Noori <xishma@gmail.com>
2025-03-24 16:31:19 -0400

    adds some new endpoints

--- generate.sh
+++ generate.sh
@@ -49,10 +49,11 @@ rm formz* icas* authentication* storage-bundeled.yaml v2.0-bundeled.yaml
 
 wget -O icas.yaml https://id.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 wget -O formz.yaml https://api.staging.formaloo.com/docs/openapi/yaml/?version=3.0
-wget -O authentication.yaml https://auth.staging.formaloo.com/docs/openapi/yaml?version=2.0
-wget -O storage.yaml https://storage.staging.formaloo.com/docs/openapi/yaml/?version=2.0
+wget -O authentication.yaml https://auth.staging.formaloo.com/docs/openapi/yaml?version=3.0
+wget -O storage.yaml https://storage.staging.formaloo.com/docs/openapi/yaml/?version=3.0
+wget -O ai.yaml https://ai.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 
-files=( icas.yml formz.yaml authentication.yaml storage.yaml )
+files=( icas.yml formz.yaml authentication.yaml storage.yaml ai.yaml )
 for file in "${files[@]}"; 
 do
     grep -o 'docs.*.md' $file | while read -r line ; 
@@ -65,10 +66,11 @@ redocly bundle icas.yaml -o icas-bundeled.yaml
 redocly bundle formz.yaml -o formz-bundeled.yaml
 redocly bundle authentication.yaml -o authentication-bundeled.yaml
 redocly bundle storage.yaml -o storage-bundeled.yaml
+redocly bundle ai.yaml -o ai-bundeled.yaml
 redocly bundle v3.0.yaml -o v3.0-bundeled.yaml
 
 npx openapi-merge-cli --config openapi-merge-v3.0.json
-rm formz* v3.0-bundeled.yaml 
+rm formz* icas* authentication* ai* storage-bundeled.yaml v3.0-bundeled.yaml 
 
 mkdir -p /files/html/ && rm -r /files/html/*
 cp /files/v*.html /files/html/
