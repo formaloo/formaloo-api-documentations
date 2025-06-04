cf0f8f6fb3f870c27d96e7675653e5d3692c9f79
Hasan Noori <xishma@gmail.com>
2025-06-04 16:26:42 -0400

    revert changes regardin ai service

--- generate.sh
+++ generate.sh
@@ -51,8 +51,9 @@ wget -O icas.yaml https://id.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 wget -O formz.yaml https://api.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 wget -O authentication.yaml https://auth.staging.formaloo.com/docs/openapi/yaml?version=3.0
 wget -O storage.yaml https://storage.staging.formaloo.com/docs/openapi/yaml/?version=3.0
+wget -O ai.yaml https://ai.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 
-files=( icas.yaml formz.yaml authentication.yaml storage.yaml )
+files=( icas.yaml formz.yaml authentication.yaml storage.yaml ai.yaml )
 for file in "${files[@]}"; 
 do
     grep -o 'docs.*.md' $file | while read -r line ; 
@@ -65,10 +66,11 @@ redocly bundle icas.yaml -o icas-bundeled.yaml
 redocly bundle formz.yaml -o formz-bundeled.yaml
 redocly bundle authentication.yaml -o authentication-bundeled.yaml
 redocly bundle storage.yaml -o storage-bundeled.yaml
+redocly bundle ai.yaml -o ai-bundeled.yaml
 redocly bundle v3.0.yaml -o v3.0-bundeled.yaml
 
 npx openapi-merge-cli --config openapi-merge-v3.0.json
-rm formz* icas* authentication* storage-bundeled.yaml v3.0-bundeled.yaml 
+rm formz* icas* authentication* ai* storage-bundeled.yaml v3.0-bundeled.yaml 
 
 mkdir -p /files/html/ && rm -r /files/html/*
 cp /files/v*.html /files/html/
