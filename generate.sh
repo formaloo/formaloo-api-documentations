d3477b230b2b723614439ae84b2825b8d1676f38
Hasan Noori <xishma@gmail.com>
2025-06-04 16:11:29 -0400

    updates api docs and removes ai for now

--- generate.sh
+++ generate.sh
@@ -51,9 +51,8 @@ wget -O icas.yaml https://id.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 wget -O formz.yaml https://api.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 wget -O authentication.yaml https://auth.staging.formaloo.com/docs/openapi/yaml?version=3.0
 wget -O storage.yaml https://storage.staging.formaloo.com/docs/openapi/yaml/?version=3.0
-wget -O ai.yaml https://ai.staging.formaloo.com/docs/openapi/yaml/?version=3.0
 
-files=( icas.yml formz.yaml authentication.yaml storage.yaml ai.yaml )
+files=( icas.yaml formz.yaml authentication.yaml storage.yaml )
 for file in "${files[@]}"; 
 do
     grep -o 'docs.*.md' $file | while read -r line ; 
@@ -66,11 +65,10 @@ redocly bundle icas.yaml -o icas-bundeled.yaml
 redocly bundle formz.yaml -o formz-bundeled.yaml
 redocly bundle authentication.yaml -o authentication-bundeled.yaml
 redocly bundle storage.yaml -o storage-bundeled.yaml
-redocly bundle ai.yaml -o ai-bundeled.yaml
 redocly bundle v3.0.yaml -o v3.0-bundeled.yaml
 
 npx openapi-merge-cli --config openapi-merge-v3.0.json
-rm formz* icas* authentication* ai* storage-bundeled.yaml v3.0-bundeled.yaml 
+rm formz* icas* authentication* storage-bundeled.yaml v3.0-bundeled.yaml 
 
 mkdir -p /files/html/ && rm -r /files/html/*
 cp /files/v*.html /files/html/
